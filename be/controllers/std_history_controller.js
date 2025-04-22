98d21c9a2fe5e7ea4eba8a15ace5bc9686954e4e
atfox272 <trananhtai577@gmail.com>
2025-04-23 00:02:43 +0700

    mod: change router handle

--- be/controllers/std_history_controller.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const stdHistoryController = require('../controllers/std_history_controller');
-
-// router.post('/<api_path>', stdHistoryController.<api_function>);
-
-module.exports = router;
