310fda6d946079061b6403961940e7856959ed3e
qk734 <kien.quachtrong734@hcmut.edu.vn>
2025-04-25 13:25:16 +0700

    add: srso room config controller

---
+++ be/controllers/srso_room_config_controller.js
@@ -0,0 +1,109 @@
+const rooms = require('../database/rooms.json');
+
+exports.getRoomByCampus = (req, res) => {
+    const { cs } = req.body;
+    const room = rooms.find(rr => rr.campus === cs);
+    if (room) {
+        res.json({ success: true, room: room});
+    } else {
+        res.status(404).json({ success: false, message: 'User not found' });
+    }
+}; 
+
+exports.getRoomById = (req, res) => {
+    const roomId = req.query.roomId;
+    const room = rooms.find(r => r.roomId === roomId);
+    if (room) {
+        res.json({ success: true, room: room });
+    } else {
+        res.status(404).json({ error: 'Room not found' });
+    }
+};
+
+exports.addRoom = (req, res) => {
+    const {campus, building, floor, room, capacity, equipment} = req.body;
+    const newRoom = {
+        campus,
+        building,
+        floor,
+        room,
+        capacity,
+        equipment
+    }
+    rooms.push(newRoom);
+    res.json({ success: true, 
+        message: "Add a new room completely"
+    });
+};
+
+
+exports.updateRoom = (req, res) => {
+    const {campus, building, floor, room} = req.body;
+    const r = rooms.find(rr => rr.campus === campus && rr.building === building && rr.floor === floor && rr.room === room  );
+    if (r) {
+        Object.assign(r, req.body);
+        res.json({success: true});
+    } else {
+        res.status(404).send('Room not found');
+    }
+};
+
+
+exports.deleteRoom = (req, res) => {
+    const {campus, building, floor, room} = req.body;
+    const rindex = rooms.findIndex(rr => rr.campus === campus && rr.building === building && rr.floor === floor && rr.room === room  );
+    if (rindex !== -1) {
+        rooms.splice(index, 1);
+        res.json({success: true});
+    } else {
+        res.status(404).send('Room not found');
+    }
+};
+
+exports.getRoomByOptions = (req, res) => {
+    res.json({
+        success: true,
+        campuses: [
+          {
+            id: "1",
+            name: "Cơ sở 1"
+          },
+          {
+            id: "2",
+            name: "Cơ sở 2"
+          }
+        ],
+        equipment: [
+          {
+            id: "wifi",
+            name: "Wifi"
+          },
+          {
+            id: "projector",
+            name: "Máy chiếu"
+          },
+          {
+            id: "charger",
+            name: "Ổ sạc"
+          },
+          {
+            id: "table",
+            name: "Bàn"
+          },
+              {
+                id: "meeting_device",
+                name: "Thiết bị họp"
+              },
+              {
+                id: "big_board",
+                name: "Bảng lớn"
+              },
+          {
+            id: "ac",
+            name: "Máy lạnh"
+          }
+        ]
+      });
+};
+
+
--- be/routes/srso_room_config.js
+++ be/routes/srso_room_config.js
@@ -2,6 +2,11 @@ const express = require('express');
 const router = express.Router();
 const srsoRoomConfigController = require('../controllers/srso_room_config_controller.js');
 
-// router.post('/add-room', srsoRoomConfigController.addRoom);
-//                                               <<Your function here>>
+router.post('/get-room-by-campus', srsoRoomConfigController.getRoomByCampus);
+router.post('/get-room-by-id', srsoRoomConfigController.getRoomById);
+router.post('/get-room-by-options', srsoRoomConfigController.getRoomByOptions);
+router.post('/add-room', srsoRoomConfigController.addRoom);
+router.post('/update-room', srsoRoomConfigController.updateRoom);
+router.post('/delete-room', srsoRoomConfigController.deleteRoom);
+
 module.exports = router;
