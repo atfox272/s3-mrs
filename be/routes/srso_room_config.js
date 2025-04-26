98c8cef200083bb304c7ac78bb1316a7604251fb
qk734 <133137136+qk734@users.noreply.github.com>
2025-04-26 09:43:37 +0700

    Update srso_room_config.js

--- be/routes/srso_room_config.js
+++ be/routes/srso_room_config.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const srsoRoomConfigController = require('../controllers/srso_room_config_controller.js');
 
-router.post('/get-room-by-campus', srsoRoomConfigController.getRoomByCampus);
-router.post('/get-room-by-id', srsoRoomConfigController.getRoomById);
-router.post('/get-room-by-options', srsoRoomConfigController.getRoomByOptions);
+router.get('/get-room-by-campus', srsoRoomConfigController.getRoomByCampus);
+router.get('/get-room-by-id', srsoRoomConfigController.getRoomById);
+router.get('/get-room-by-options', srsoRoomConfigController.getRoomByOptions);
 router.post('/add-room', srsoRoomConfigController.addRoom);
 router.post('/update-room', srsoRoomConfigController.updateRoom);
 router.post('/delete-room', srsoRoomConfigController.deleteRoom);
